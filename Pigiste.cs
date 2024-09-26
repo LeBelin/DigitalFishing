ddb74040d8cd68e70cc485faab2387fade0fd033
Le Belin <117570222+LeBelin@users.noreply.github.com>
2024-09-26 22:44:59 +0200

    Add files via upload

---
+++ Pigiste.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalFishing
+{
+    public class Pigiste
+    {
+
+        private string _nom;
+        private double _salaire;
+
+        public double Salaire
+        {
+            get { return _salaire; }
+            set { _salaire = value; }
+        }
+
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public Pigiste(string nm, double sl)
+        {
+            _nom = nm;
+            _salaire = sl;
+        }
+
+        public double calcRemu()
+        {
+            return _salaire * 0.8;
+        }
+    }
+}
